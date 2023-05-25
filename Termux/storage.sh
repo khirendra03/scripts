d5e4c958c622d6a9a2624667f9fd799b1a1527e5
khirendra <khirendrapatel3@gmail.com>
2023-05-25 13:45:38 +0530

    updating storeage.sh
    - Enclosed variables in double quotes to handle paths with spaces and prevent word splitting.
    - Added a case statement for command-line argument handling, which is more readable and maintainable.
    - Improved the function scrp() to accept arguments for file pattern and column number, making it more reusable.
    - Added comments to clarify the purpose of each section.
    - Updated variable names for clarity and consistency.
    - Fixed potential issues with quoting and redirection.
    
    Signed-off-by: khirendra <khirendrapatel3@gmail.com>

--- Termux/storage.sh
+++ Termux/storage.sh
@@ -3,42 +3,39 @@
 ## some devices might be different, specify on your own
 
 # Internal and external storage paths
+#internal="/storage/emulated/0/Android/data"
 internal=/storage/emulated/0/Android/data
-external=/storage/D639-79B5 # your external card's name
+#external="/storage/D639-79B5"  # your external card's name
+external=/storage/BD6F-160B
 
 # Temporary file to store output
-ftmp=$HOME/.scripts/util_data/data.tmp
+ftmp="$HOME/.scripts/util_data/data.tmp"
 
 # Gather storage information using 'df -h' command and redirect output to $ftmp file
-df -h 2>/dev/null > $ftmp
+df -h 2>/dev/null > "$ftmp"
 
-# Function to extract specific columns from the internal storage information
+# Function to extract specific columns from the storage information
 scrp(){
-  grep $internal $ftmp | awk "{print \$$1}"
-}
-
-# Function to extract specific columns from the external storage information
-scrpX(){
-  grep $external $ftmp | awk "{print \$$1}"
+  grep "$1" "$ftmp" | awk "{print \$$2}"
 }
 
 ## internal info
 # Obtain internal storage information using the 'scrp' function and assign to variables
-Fsys=$(scrp 1)    # Filesystem
-Size=$(scrp 2)    # Total size
-Used=$(scrp 3)    # Used space
-Avai=$(scrp 4)    # Available space
-Perc=$(scrp 5)    # Usage percentage
-Moun=$(scrp 6)    # Mount point
+Fsys=$(scrp "$internal" 1)    # Filesystem
+Size=$(scrp "$internal" 2)    # Total size
+Used=$(scrp "$internal" 3)    # Used space
+Avai=$(scrp "$internal" 4)    # Available space
+Perc=$(scrp "$internal" 5)    # Usage percentage
+Moun=$(scrp "$internal" 6)    # Mount point
 
 # external info
-# Obtain external storage information using the 'scrpX' function and assign to variables
-FsysX=$(scrpX 1)  # Filesystem
-SizeX=$(scrpX 2)  # Total size
-UsedX=$(scrpX 3)  # Used space
-AvaiX=$(scrpX 4)  # Available space
-PercX=$(scrpX 5)  # Usage percentage
-MounX=$(scrpX 6)  # Mount point
+# Obtain external storage information using the 'scrp' function and assign to variables
+FsysX=$(scrp "$external" 1)  # Filesystem
+SizeX=$(scrp "$external" 2)  # Total size
+UsedX=$(scrp "$external" 3)  # Used space
+AvaiX=$(scrp "$external" 4)  # Available space
+PercX=$(scrp "$external" 5)  # Usage percentage
+MounX=$(scrp "$external" 6)  # Mount point
 
 # Formatted string containing internal storage information
 InAll="
@@ -69,25 +66,30 @@ fetch_info(){
 
 # Store the storage usage information for internal and external storage into $ftmp file
 data="
-inter $(fetch_info $Used $Size $Perc)
-exter $(fetch_info $UsedX $SizeX $PercX)
-" &> $ftmp
+inter $(fetch_info "$Used" "$Size" "$Perc")
+exter $(fetch_info "$UsedX" "$SizeX" "$PercX")
+" &> "$ftmp"
 
 # Check the value of the first command-line argument to determine the desired output
-if [[ "$1" == "-a" ]]; then
-  echo "$data"   # Print both internal and external storage usage information
-elif [[ "$1" == "-i" ]]; then
-  fetch_info $Used $Size $Perc    # Print internal storage usage information
-elif [[ "$1" == "-e" ]]; then
-  fetch_info $UsedX $SizeX $PercX  # Print external storage usage information
-elif [[ "$1" == "-I" ]]; then
-  echo -e "$InAll"   # Print full internal storage information
-elif [[ "$1" == "-E" ]]; then
-  echo -e "$ExAll"   # Print full external storage information
-else
-  {
-    echo -e "USAGE: ./storage.sh -[i|e|I|E]";
-    echo -e "e.g: ./storage.sh -I";
-    exit 1;
-  }
-fi
+case "$1" in
+  -a)
+    echo "$data"   # Print both internal and external storage usage information
+    ;;
+  -i)
+    fetch_info "$Used" "$Size" "$Perc"    # Print internal storage usage information
+    ;;
+  -e)
+    fetch_info "$UsedX" "$SizeX" "$PercX"  # Print external storage usage information
+    ;;
+  -I)
+    echo -e "$InAll"   # Print full internal storage information
+    ;;
+  -E)
+    echo -e "$ExAll"   # Print full external storage information
+    ;;
+  *)
+    echo -e "USAGE: ./storage.sh -[i|e|I|E]"
+    echo -e "e.g: ./storage.sh -I"
+    exit 1
+    ;;
+esac
