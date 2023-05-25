21205420325513820e4aa6b17527ea6d48244076
khirendra <khirendrapatel3@gmail.com>
2023-05-25 13:29:58 +0530

    updating rename.sh
    Signed-off-by: khirendra <khirendrapatel3@gmail.com>

--- Termux/rename.sh
+++ Termux/rename.sh
@@ -1,5 +1,18 @@
-#bin/vash
+#!/bin/bash
 
-# removing Angus-
+# Change to the directory containing the files
+cd /path/to/files
 
-for i in *;do mv $i `echo $i | tr -d "Angus-"`;done
+# Specify the pattern to match
+pattern="oldfile_*.txt"
+
+# Loop through each file matching the pattern
+for file in $pattern; do
+  # Extract the filename without the prefix
+  new_name="${file#oldfile_}"
+  
+  # Rename the file
+  mv "$file" "newfile_$new_name"
+done
+
+echo "Files renamed successfully."
