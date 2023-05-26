c35e9f4e0a98229ece5069e1f3a0424479a1ad5e
khirendra <khirendrapatel3@gmail.com>
2023-05-26 20:58:44 +0530

    font: update the script
    Signed-off-by: khirendra <khirendrapatel3@gmail.com>

--- Termux/font.sh
+++ Termux/font.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# Error handling function
+handle_error() {
+  echo "An error occurred. Exiting."
+  exit 1
+}
+
 # Prompt user for font or emoji name
 read -p "Enter the font or emoji name: " search_query
+
+# Validate input
+if [[ -z $search_query ]]; then
+  echo "Invalid input. Font or emoji name is required."
+  exit 1
+fi
 
 # Function to search for files within a website
 search_website() {
@@ -9,7 +21,7 @@ search_website() {
   local pattern=$2
 
   # Retrieve the website's HTML content
-  html=$(curl -s "$website")
+  html=$(curl -s "$website") || handle_error
 
   # Search for files matching the pattern
   files=$(echo "$html" | grep -E -o "$pattern" | awk '{print $0}')
@@ -23,45 +35,53 @@ search_website() {
   fi
 }
 
-# Function to search for repositories on GitHub
-search_github() {
-  local query=$1
+# Function to search for repositories on a platform
+search_platform() {
+  local platform=$1
+  local url=$2
+  local query=$3
 
-  # Query GitHub API for repositories
-  results=$(curl -s "https://api.github.com/search/repositories?q=$query")
+  # Query platform API for repositories
+  results=$(curl -s "$url") || handle_error
 
   # Extract repository URLs
-  repo_urls=$(echo "$results" | grep -E -o "https://github.com/[^\"]+")
+  repo_urls=$(echo "$results" | grep -E -o "https://$platform.com/[^\"]+")
 
   # Output repository URLs
   if [[ -n $repo_urls ]]; then
-    echo "Repositories found on GitHub:"
+    echo "Repositories found on $platform:"
     echo "$repo_urls"
   else
-    echo "No repositories found on GitHub."
+    echo "No repositories found on $platform."
   fi
 }
 
-# Function to search for repositories on GitLab
-search_gitlab() {
+# Function to search for font files on the internet
+search_internet() {
   local query=$1
 
-  # Query GitLab API for repositories
-  results=$(curl -s "https://gitlab.com/api/v4/projects?search=$query")
+  # Perform a web search on Google
+  search_results=$(curl -s "https://www.google.com/search?q=$query+filetype:ttf") || handle_error
 
-  # Extract repository URLs
-  repo_urls=$(echo "$results" | grep -E -o "https://gitlab.com/[^\"]+")
+  # Extract URLs from search results
+  urls=$(echo "$search_results" | grep -E -o "https?://[^\"]+")
 
-  # Output repository URLs
-  if [[ -n $repo_urls ]]; then
-    echo "Repositories found on GitLab:"
-    echo "$repo_urls"
+  # Output search results
+  if [[ -n $urls ]]; then
+    echo "Font files found on the internet:"
+    echo "$urls"
   else
-    echo "No repositories found on GitLab."
+    echo "No font files found on the internet."
   fi
 }
 
-# List of websites to search from
+# List of platforms to search from
+platforms=(
+  "GitHub,https://api.github.com/search/repositories?q=$search_query"
+  "GitLab,https://gitlab.com/api/v4/projects?search=$search_query"
+)
+
+# Search font files on websites
 websites=(
   "https://fonts.google.com"
   "https://www.dafont.com"
@@ -75,18 +95,22 @@ websites=(
   "https://www.urbanfonts.com"
 )
 
-# Search font files on websites
 for site in "${websites[@]}"; do
   echo "Searching files from: $site"
   search_website "$site" "$search_query"
   echo
 done
 
-# Search GitHub for font repositories
-echo "Searching GitHub for font repositories..."
-search_github "$search_query"
-echo
+# Search repositories on platforms
+for platform in "${platforms[@]}"; do
+  platform_name=$(echo "$platform" | cut -d',' -f1)
+  platform_url=$(echo "$platform" | cut -d',' -f2)
+  echo "Searching $platform_name for repositories..."
+  search_platform "$platform_name" "$platform_url" "$search_query"
+  echo
+done
 
-# Search GitLab for font repositories
-echo "Searching GitLab for font repositories..."
-search_gitlab "$search_query"
+# Search font files on the internet
+echo "Searching the internet for font files..."
+search_internet "$search_query"
+echo
