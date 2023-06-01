82037fa5cfed0c3490d2dd75e679d28a1d086604
khirendra <khirendrapatel3@gmail.com>
2023-06-01 10:41:15 +0530

    adding few stuff
    Signed-off-by: khirendra <khirendrapatel3@gmail.com>

---
+++ others/zero-theme.sh
@@ -0,0 +1,7 @@
+
+
+git clone https://github.com/khirendra03/zero.zsh                                                   7640  cd zero.zsh
+ls
+mv zero.zsh-theme $ZSH_CUSTOM/themes/zero.zsh-theme
+cd $ZSH_CUSTOM/themes
+ls
